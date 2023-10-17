003b16019f6b5a0b4127ca6c9fffc26b986ae4c2
KaharErbol <kaharerbol@gmail.com>
2023-10-17 13:54:19 -0700

    App created

---
+++ fetch-dog-app/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Login from './Login.jsx'
+
+function App() {
+
+  return (
+    <>
+      <div>Fetch Dogs</div>
+      <div className='login-container'>
+        <Login />
+      </div>
+    </>
+  )
+}
+
+export default App;
---
+++ fetch-dog-app/src/Login.jsx
@@ -0,0 +1,19 @@
+
+
+function Login() {
+
+
+    return (
+        <>
+          <div className="login-page">
+            <div>Login Page</div>
+            <div className="login-inputs">
+              <input type="text" placeholder="name"/>
+              <input type="text" name="" id="" placeholder="email"/>
+            </div>
+          </div>
+        </>
+    )
+}
+
+export default Login;
